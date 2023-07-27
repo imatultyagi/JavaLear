d2554fb7bf40e14b5019a8e83a9a51148e469f3e
imatultyagi <atutyagig85@gmail.com>
2023-07-27 11:46:20 +0530

    Table

--- src/Add.java
+++ src/Add.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Add {
     public Add(){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a no to print table");
+        int n = sc.nextInt();
+        for (int i = 1; i < 11; i++) {
+            System.out.println(n*i);
+        }
     }
 }
